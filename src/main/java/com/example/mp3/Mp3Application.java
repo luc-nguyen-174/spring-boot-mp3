1f39cfc4927d240e2700ea56586e8ae610b72e70
luc-nguyen-174 <louiskingvn@gmail.com>
2023-04-13 13:50:27 +0700

    first commit

---
+++ src/main/java/com/example/mp3/Mp3Application.java
@@ -0,0 +1,13 @@
+package com.example.mp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mp3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mp3/Mp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mp3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
