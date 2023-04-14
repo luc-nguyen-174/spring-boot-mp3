13b9950c8df7653aae5193fd30818d050304c3cb
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-04-14 10:23:53 +0700

    CRUD Singer

---
+++ src/main/java/com/example/mp3/controller/SingerController.java
@@ -0,0 +1,38 @@
+package com.example.mp3.controller;
+
+import com.example.mp3.model.music.Singer;
+import com.example.mp3.service.music.ISingerService;
+import org.aspectj.apache.bcel.classfile.Module;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/singers")
+public class SingerController {
+    @Autowired
+    private ISingerService singerService;
+
+    @GetMapping("/")
+    public ResponseEntity<Iterable<Singer>> showListSinger(){
+        return ResponseEntity.ok(singerService.findAll());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Singer> createSinger(@RequestBody Singer singer){
+        return ResponseEntity.ok(singerService.save(singer));
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity<Singer> deleteSinger(@PathVariable Long id){
+        Optional<Singer> optionalSinger =singerService.findById(id);
+        if(optionalSinger.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        singerService.remove(id);
+        return new ResponseEntity<>(optionalSinger.get(), HttpStatus.NO_CONTENT);
+    }
+}
