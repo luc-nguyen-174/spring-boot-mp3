ba150b8b042ff0d9f2482bbf641120983c281231
luc-nguyen-174 <louiskingvn@gmail.com>
2023-04-14 14:08:16 +0700

    done model

--- src/main/java/com/example/mp3/model/music/Music.java
+++ src/main/java/com/example/mp3/model/music/Music.java
@@ -1,6 +1,10 @@
 package com.example.mp3.model.music;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Table(name = "musics")
@@ -8,12 +12,138 @@ public class Music {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String musicName;
+    private String description;
+    private String fileName;
+    private String imageName;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "musics_singers",
+            joinColumns = {@JoinColumn(name = "music_id")},
+            inverseJoinColumns = {@JoinColumn(name = "singers_id")}
+    )
+//    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Set<Singer> singers;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "musics_kinds",
+            joinColumns = {@JoinColumn(name = "music_id")},
+            inverseJoinColumns = {@JoinColumn(name = "kinds_id")}
+    )
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Set<Kind> kinds;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "musics_albums",
+            joinColumns = {@JoinColumn(name = "music_id")},
+            inverseJoinColumns = {@JoinColumn(name = "kinds_id")}
+    )
+    private Set<Album> albums;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "musics_author",
+            joinColumns = {@JoinColumn(name = "music_id")},
+            inverseJoinColumns = {@JoinColumn(name = "author_id")}
+    )
+    private Set<Author> authors;
+
+    @ManyToMany(mappedBy = "musics")
+    private Set<Playlist> playlists;
+
+    private LocalDateTime uploadTime;
+
+
+    public Music() {
+    }
+
+    public Long getId() {
+        return id;
+    }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getId() {
-        return id;
+    public String getMusicName() {
+        return musicName;
+    }
+
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public Set<Singer> getSingers() {
+        return singers;
+    }
+
+    public void setSingers(Set<Singer> singers) {
+        this.singers = singers;
+    }
+
+    public Set<Kind> getKinds() {
+        return kinds;
+    }
+
+    public void setKinds(Set<Kind> kinds) {
+        this.kinds = kinds;
+    }
+
+    public Set<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(Set<Album> albums) {
+        this.albums = albums;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public LocalDateTime getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(LocalDateTime uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public Set<Playlist> getPlaylists() {
+        return playlists;
+    }
+
+    public void setPlaylists(Set<Playlist> playlists) {
+        this.playlists = playlists;
     }
 }
--- src/main/java/com/example/mp3/model/user/AppUser.java
+++ src/main/java/com/example/mp3/model/user/AppUser.java
@@ -1,6 +1,7 @@
 package com.example.mp3.model.user;
 
-import com.example.mp3.model.music.PlayList;
+
+import com.example.mp3.model.music.Playlist;
 
 import javax.persistence.*;
 import java.util.List;
@@ -26,8 +27,8 @@ public class AppUser {
     private String email;
     private String phone;
     private String avatar;
-    @OneToMany(mappedBy = "users")
-    private List<PlayList> playLists;
+    @ManyToMany(mappedBy = "users")
+    private List<Playlist> playlists;
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles", joinColumns = {@JoinColumn(name = "user_id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id")})
@@ -128,11 +129,11 @@ public class AppUser {
         this.roleSet = roleSet;
     }
 
-    public List<PlayList> getPlayLists() {
-        return playLists;
+    public List<Playlist> getPlaylists() {
+        return playlists;
     }
 
-    public void setPlayLists(List<PlayList> playLists) {
-        this.playLists = playLists;
+    public void setPlaylists(List<Playlist> playlists) {
+        this.playlists = playlists;
     }
 }
