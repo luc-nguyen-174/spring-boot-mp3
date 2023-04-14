50803015df8b57f8abea48f3d6e61e4c4655b23f
luc-nguyen-174 <louiskingvn@gmail.com>
2023-04-14 13:40:16 +0700

    Revert "update music model"
    This reverts commit 4b9605a68bed351af30285908073e4eb516b1456.

--- src/main/java/com/example/mp3/model/music/Music.java
+++ src/main/java/com/example/mp3/model/music/Music.java
@@ -1,11 +1,6 @@
 package com.example.mp3.model.music;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Set;
 
 @Entity
 @Table(name = "musics")
@@ -13,125 +8,12 @@ public class Music {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String musicName;
-    private String description;
-    private String fileName;
-    private String imageName;
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "musics_singers",
-            joinColumns = {@JoinColumn(name = "music_id")},
-            inverseJoinColumns = {@JoinColumn(name = "singers_id")}
-    )
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    private Set<Singer> singers;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "musics_kinds",
-            joinColumns = {@JoinColumn(name = "music_id")},
-            inverseJoinColumns = {@JoinColumn(name = "kinds_id")}
-    )
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    private Set<Kind> kinds;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "musics_albums",
-            joinColumns = {@JoinColumn(name = "music_id")},
-            inverseJoinColumns = {@JoinColumn(name = "kinds_id")}
-    )
-    private Set<Album> albums;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "musics_author",
-            joinColumns = {@JoinColumn(name = "music_id")},
-            inverseJoinColumns = {@JoinColumn(name = "author_id")}
-    )
-    private Set<Author> authors;
-
-    private LocalDateTime uploadTime;
-    public Music() {
-    }
-
-    public Long getId() {
-        return id;
-    }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getMusicName() {
-        return musicName;
-    }
-
-    public void setMusicName(String musicName) {
-        this.musicName = musicName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getImageName() {
-        return imageName;
-    }
-
-    public void setImageName(String imageName) {
-        this.imageName = imageName;
-    }
-
-    public Set<Singer> getSingers() {
-        return singers;
-    }
-
-    public void setSingers(Set<Singer> singers) {
-        this.singers = singers;
-    }
-
-    public Set<Kind> getKinds() {
-        return kinds;
-    }
-
-    public void setKinds(Set<Kind> kinds) {
-        this.kinds = kinds;
-    }
-
-    public Set<Album> getAlbums() {
-        return albums;
-    }
-
-    public void setAlbums(Set<Album> albums) {
-        this.albums = albums;
-    }
-
-    public Set<Author> getAuthors() {
-        return authors;
-    }
-
-    public void setAuthors(Set<Author> authors) {
-        this.authors = authors;
-    }
-
-    public LocalDateTime getUploadTime() {
-        return uploadTime;
-    }
-
-    public void setUploadTime(LocalDateTime uploadTime) {
-        this.uploadTime = uploadTime;
+    public Long getId() {
+        return id;
     }
 }
