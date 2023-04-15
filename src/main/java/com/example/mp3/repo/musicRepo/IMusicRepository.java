5695b5630d798906a23522840e7945f642cf54cf
luc-nguyen-174 <louiskingvn@gmail.com>
2023-04-16 02:00:22 +0700

    update music repo with search

--- src/main/java/com/example/mp3/repo/musicRepo/IMusicRepository.java
+++ src/main/java/com/example/mp3/repo/musicRepo/IMusicRepository.java
@@ -1,13 +1,15 @@
 package com.example.mp3.repo.musicRepo;
 
-import com.example.mp3.model.music.Author;
 import com.example.mp3.model.music.Music;
 import com.example.mp3.model.music.Singer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Set;
 
-public interface IMusicRepository extends JpaRepository<Music,Long> {
-    Iterable<Music> findAllByMusicNameOrSingersOrAuthors(String musicName, Set<Singer> singers, Set<Author> authors);
+public interface IMusicRepository extends JpaRepository<Music, Long> {
+    Iterable<Music> findAllByMusicNameContainingIgnoreCaseOrSingersContainingIgnoreCaseOrAuthorsNotContainingIgnoreCase(
+            String musicName, Singer singers, String authors);
+
+    Iterable<Music> findAllByAlbumsContainingIgnoreCase(String album);
 
 }
