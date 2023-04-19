4513a79ad462ea972efbef5c6c00b2c315bb1400
luc-nguyen-174 <louiskingvn@gmail.com>
2023-04-19 09:43:48 +0700

    update model

--- src/main/java/com/example/mp3/model/music/MusicForm.java
+++ src/main/java/com/example/mp3/model/music/MusicForm.java
@@ -3,6 +3,7 @@ package com.example.mp3.model.music;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 public class MusicForm {
     private Long id;
@@ -13,6 +14,7 @@ public class MusicForm {
     private String albums;
     private String authors;
     private LocalDateTime uploadTime;
+    private Set<Singer> singers;
 
     public MusicForm() {
     }
